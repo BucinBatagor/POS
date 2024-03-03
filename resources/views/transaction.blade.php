11d6895ffa35a5b6b22a3719d030a3300cc265fa
Nizar Khawarizmi <nizar.banme@gmail.com>
2024-03-03 19:05:23 +0700

    Membuat transaksi

---
+++ resources/views/transaction.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Transaction History</title>
+</head>
+<body>
+    <h1>Transaction History</h1>
+    <p><a href="http://localhost/POS/public/home">Back</a></p>
+</body>
+</html>
